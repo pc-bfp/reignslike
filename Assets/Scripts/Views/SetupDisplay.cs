fe0b301b3607786a042cbcb831d946bc00ba99be
Projjal Chakrabarti <projjalc@gmail.com>
2020-01-21 01:54:34 +0530

    Updated to Unity 2019.2  - Added remaining doodles

--- Assets/Scripts/Views/SetupDisplay.cs
+++ Assets/Scripts/Views/SetupDisplay.cs
@@ -6,6 +6,8 @@ public class SetupDisplay : MonoBehaviour {
 	[SerializeField] List<int> turnsList, startValueList;
 	[SerializeField] List<string> statTypeList;
 
+	public delegate void ListValueChanged<T>(ref List<T> list, T newValue);
+
 	void Start() {
 
 	}
